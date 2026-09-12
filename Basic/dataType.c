bd0a66ad7c70c87934be7c5469fe17da50400981
adil4260 <adilsarkar001@gmail.com>
2026-09-12 22:29:19 +0600

    first commit

---
+++ Basic/dataType.c
@@ -0,0 +1,12 @@
+// write a program that print an integer number
+
+#include <stdio.h>
+int main()
+{
+    // int num1 = 10, num2 = 30;
+    int num1 = 10;
+    int num2 = 30;
+    printf("Number is %d \n", num1);
+    printf("Number2 is %d", num2);
+    printf("Number are %d ,%d \n", num1, num2);
+}
---
+++ Basic/double-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1 = 10.4;
+    double num2 = 10.222534;
+    char ch = 'a';
+
+    printf("num1 = %f\n", num1);
+    printf("num2 = %lf\n", num2);
+    printf("ch = %c\n", ch);
+
+    return 0;
+}
